5d5e9f17329378076b58a7f4695626a1a72fa32f
phamgialinhlx <phamgialinhlx2@gmail.com>
2023-10-02 22:12:47 +0700

    Complete ngram with backoff and interpolation

--- ngram.py
+++ ngram.py
@@ -4,24 +4,24 @@ import spacy
 
 nlp = spacy.load('en_core_web_sm')
 
-corpus = (
-    "Gus Proto is a Python developer currently"
-    " working for a London-based Fintech company. He is"
-    " interested in learning Natural Language Processing."
-    " There is a developer conference happening on 21 July"
-    ' 2019 in London. It is titled "Applications of Natural'
-    ' Language Processing". There is a helpline number'
-    " available at +44-1234567891. Gus is helping organize it."
-    " He keeps organizing local Python meetups and several"
-    " internal talks at his workplace. Gus is also presenting"
-    ' a talk. The talk will introduce the reader about "Use'
-    ' cases of Natural Language Processing in Fintech".'
-    " Apart from his work, he is very passionate about music."
-    " Gus is learning to play the Piano. He has enrolled"
-    " himself in the weekend batch of Great Piano Academy."
-    " Great Piano Academy is situated in Mayfair or the City"
-    " of London and has world-class piano instructors."
-)
+# corpus = (
+#     "Gus Proto is a Python developer currently"
+#     " working for a London-based Fintech company. He is"
+#     " interested in learning Natural Language Processing."
+#     " There is a developer conference happening on 21 July"
+#     ' 2019 in London. It is titled "Applications of Natural'
+#     ' Language Processing". There is a helpline number'
+#     " available at +44-1234567891. Gus is helping organize it."
+#     " He keeps organizing local Python meetups and several"
+#     " internal talks at his workplace. Gus is also presenting"
+#     ' a talk. The talk will introduce the reader about "Use'
+#     ' cases of Natural Language Processing in Fintech".'
+#     " Apart from his work, he is very passionate about music."
+#     " Gus is learning to play the Piano. He has enrolled"
+#     " himself in the weekend batch of Great Piano Academy."
+#     " Great Piano Academy is situated in Mayfair or the City"
+#     " of London and has world-class piano instructors."
+# )
 corpus = (
     "The boy likes books. The"
     " books are bank"
@@ -32,55 +32,84 @@ corpus = (
 complete_doc = nlp(corpus)
 
 class NGramModel:
-    def __init__(self, sentences, n):
+    def __init__(self, sentences, n, backoff=False, interpolation=False):
+        assert not (backoff == True and interpolation == True), "Can't have both backoff and interpolation"
+        self.backoff = backoff
+        self.interpolation = interpolation
+        self.full_context = backoff or interpolation
+        if self.full_context:
+            self.n = 3
+        else:
+            self.n = n
         self.build_ngrams(sentences, n)
-        self.n = n
 
     def build_ngrams(self, sentences, n):
-        self.v = len(set([token.lemma_ for token in complete_doc if not token.is_punct]))
+        self.v = set([token.lemma_ for token in complete_doc if not token.is_punct])
         batches = []
         for sentence in sentences:
             tokenized_sentence = nlp(sentence)
             words = [token.lemma_ for token in tokenized_sentence if not token.is_punct]
             words = ['<s>'] + words + ['</s>']
-            for i in range(len(words)-n+1):
-                batches.append(words[i:i+n])
+            if self.full_context:
+                for i in range(1, 4):
+                    for j in range(len(words)-i+1):
+                        batches.append(words[j:j+i])
+            else:
+                for i in range(len(words)-n+1):
+                    batches.append(words[i:i+n])
+            
         self.ngrams = {}
         for batch in batches:
             key = ' '.join(batch[:-1])
             if key not in self.ngrams.keys():
-                self.ngrams[key] = []
-            self.ngrams[key].append(batch[-1])        
+                self.ngrams[key] = {}
+            self.ngrams[key].update({batch[-1]: self.ngrams[key].get(batch[-1], 0) + 1})        
 
     def __str__(self):
         return str(self.ngrams)
-    
-    def mle(self, word, context):
+
+    def prob(self, word, context, laplace:float=1):
+        """
+        laplace=0: no smoothing
+        """
         if context not in self.ngrams.keys():
-            return 0
+            return 1 * laplace / len(self.v)
         count = Counter(self.ngrams[context])
-        return count[word] / sum(count.values())
-    
-    def mle_smoothing(self, word, context):
-        if context not in self.ngrams.keys():
-            return 1 / self.v
-        count = Counter(self.ngrams[context])
-        return (count[word] + 1) / (sum(count.values()) + self.v)
-    
+        if not self.backoff and self.interpolation:
+            return (self.ngrams[context][word] + 1 * laplace) / (sum(count.values()) + len(self.v) * laplace)
+        elif self.backoff:
+            divided_context = [token.lemma_ for token in nlp(context) if not token.is_punct]
+            backoff_context = [context, divided_context[0], ""]
+            for bc in backoff_context:
+                if bc in self.ngrams.keys():
+                    return (self.ngrams[context][word] + 1 * laplace) / (sum(count.values()) + len(self.v) * laplace)
+        elif self.interpolation:
+            divided_context = [token.lemma_ for token in nlp(context) if not token.is_punct]
+            interpolation_context = [context, divided_context[0], ""]
+            p = 0.0
+            for i in range(len(interpolation_context)):
+                p += (self.ngrams[interpolation_context[i]][word] + 1 * laplace) / (sum(count.values()) + len(self.v) * laplace)
+            return p
     def generate(self, context='<s>', length=10):
         if context == '<s>':
             sentence = [context]
         else:
             tokenized_context = nlp(context)
             sentence = [token.lemma_ for token in tokenized_context if not token.is_punct]
+
+        if self.n == 1:
+            return context + ' ' + ' '.join([max(Counter(self.ngrams[""]))for i in range(length)])
         
-        context = ' '.join(sentence[-self.n+1:])
+        context = ' '.join(sentence[-self.n + 1:])
         for i in range(length):
-            word = max(Counter(self.ngrams[context]))
+            # word = max(self.prob(word, context) for word in self.ngrams[context])
+            tmp = {word: self.prob(word, context) for word in self.ngrams[context]}
+            word = max(tmp, key=tmp.get)
+
             if word == '</s>':
                 break
             sentence.append(word)
-            context = ' '.join(sentence[-self.n+1:])
+            context = ' '.join(sentence[-self.n + 1:])
         if sentence[0] == '<s>':
             sentence = sentence[1:]
         return ' '.join(sentence)
@@ -88,18 +117,20 @@ class NGramModel:
 def main(args):
     n = args.n
     sentences = [sent.text for sent in complete_doc.sents]
-    ngrams = NGramModel(sentences, n)
+    ngrams = NGramModel(sentences, n, args.backoff, args.interpolation)
     print(ngrams.ngrams)
-    print(ngrams.mle("book", "the"))
-    print(ngrams.mle("book", "as"))
-    print(ngrams.mle_smoothing("book", "the"))
-    print(ngrams.mle_smoothing("book", "as"))
-    print(ngrams.v)
-    print(ngrams.generate(context="The books are", length=10))
+    print("Vocabs:", ngrams.v)
+    print("Probabilities of word 'book' given context 'the' (no smoothing):", ngrams.prob("book", "the", laplace=0))
+    print("Probabilities of word 'book' given context 'as' (no smoothing):", ngrams.prob("book", "as", laplace=0))
+    print("Probabilities of word 'book' given context 'the' (smoothing factor = 1)", ngrams.prob("book", "the"))
+    print("Probabilities of word 'book' given context 'as' (smoothing factor = 1)", ngrams.prob("book", "as"))
+    print("Generation with context 'The book are':",ngrams.generate(context="The books are", length=10))
 
 if __name__ == '__main__':
     # create argument parser object
     argumentParser = ArgumentParser(description='ngram model')
     argumentParser.add_argument('--n', type=int, default=2)
+    argumentParser.add_argument('--backoff', action='store_true')
+    argumentParser.add_argument('--interpolation', action='store_true')
     args = argumentParser.parse_args()
     main(args)
